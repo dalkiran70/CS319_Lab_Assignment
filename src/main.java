f2d1993708cf4fc16c74f6aee812bec814dcd65a
dalkiran70 <yumurcak232@gmail.com>
2020-04-07 12:06:05 +0300

    Game is implemented in Main class

--- src/main.java
+++ src/main.java
@@ -21,6 +21,24 @@ public class main {
         ArrayOrganizer organizer = new ArrayOrganizer();
         ArrayList<Integer> result = organizer.concatAndSort(ArrayList1, ArrayList2, ArrayList3);
         System.out.println(result);
+        Player first = new Player("Tom");
+        Player second = new Player("Jhon");
+        Player third = new Player("James");
+        Player forth = new Player("Henry");
+        Die die = new Die();
+        for (int i = 0; i < 5; i++){
+            first.addScore(die.rollDie());
+            second.addScore(die.rollDie());
+            third.addScore(die.rollDie());
+            forth.addScore(die.rollDie());
+        }
+        System.out.println(first.getName() + " has " + first.getScore() + " points.");
+        System.out.println(second.getName() + " has " + second.getScore() + " points.");
+        System.out.println(third.getName() + " has " + third.getScore() + " points.");
+        System.out.println(forth.getName() + " has " + forth.getScore() + " points.");
+
+
     }
+
 
 }
